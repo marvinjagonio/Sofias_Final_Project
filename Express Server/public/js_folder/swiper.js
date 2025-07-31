b65a6a50476ca8840003934ac365e12e48de3b29
Sofia May <tempsofimae@gmail.com>
2025-07-31 17:21:27 +0800

    31/07/25 5:21

--- Express Server/public/js_folder/swiper.js
+++ Express Server/public/js_folder/swiper.js
@@ -1,160 +1,167 @@
-
-  window.addEventListener('load', function () {
-    const comments_swiper = new Swiper('.comments_swiper', {
-      loop: false,
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.comments_next',
-        prevEl: '.comments_prev',
+window.addEventListener("load", function () {
+  const comments_swiper = new Swiper(".comments_swiper", {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".comments_next",
+      prevEl: ".comments_prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
       },
-      breakpoints: {
-        0:{
-          slidesPerView: 2,
-          
-        },
-         576: {
-         slidesPerView: 2,
-    
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        }
+      576: {
+        slidesPerView: 2,
       },
-      on:{
-        fromEdge: function () {
-        document.querySelector('.comments_prev').classList.remove('swiper-button-disabled');
-        document.querySelector('.comments_next').classList.remove('swiper-button-disabled');
-      }
-      }
-    });
-
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+    on: {
+      fromEdge: function () {
+        document
+          .querySelector(".comments_prev")
+          .classList.remove("swiper-button-disabled");
+        document
+          .querySelector(".comments_next")
+          .classList.remove("swiper-button-disabled");
+      },
+    },
   });
+});
 
- window.addEventListener('load', function () {
-const new_arrivals_swiper = new Swiper('.new_arrivals_swiper', {
-  loop: false,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  
-  navigation: {
-    nextEl: '.new-arrivals-next',
-    prevEl: '.new-arrivals-prev',
-  },
-  breakpoints: {
-        0:{
-          slidesPerView: 1,
-        },
-         576: {
-      slidesPerView: 2,
-     
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1400: {
-      slidesPerView: 5,
-    }
-  },
-   on:{
-        fromEdge: function () {
-        document.querySelector('.new-arrivals-prev').classList.remove('swiper-button-disabled');
-        document.querySelector('.new-arrivals-next').classList.remove('swiper-button-disabled');
-      }
-      }
-    });
-   
- });
+window.addEventListener("load", function () {
+  const new_arrivals_swiper = new Swiper(".new_arrivals_swiper", {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
 
- window.addEventListener('load', function () {
-const top_sellers_swiper = new Swiper('.top_sellers_swiper', {
-  loop: false,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  
-  navigation: {
-    nextEl: '.top-sellers-next',
-    prevEl: '.top-sellers-prev',
-  },
-  breakpoints: {
-        0:{
-          slidesPerView: 1,
-        },
-         576: {
-      slidesPerView: 2,
-     
+    navigation: {
+      nextEl: ".new-arrivals-next",
+      prevEl: ".new-arrivals-prev",
     },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1400: {
-      slidesPerView: 5,
-    }
-  },
-  on:{
-        fromEdge: function () {
-        document.querySelector('.top-sellers-prev').classList.remove('swiper-button-disabled');
-        document.querySelector('.top-sellers-next').classList.remove('swiper-button-disabled');
-      }
-      }
-    });
- 
-  });
-
- window.addEventListener('load', function () {
-    const featured_products_swiper = new Swiper('.featured_products_swiper', {
-      loop: false,
-     
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.featured_products_next',
-        prevEl: '.featured_products_prev',
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
       },
-      breakpoints: {
-        0:{
-          slidesPerView: 1,
-        },
-         576: {
-      slidesPerView: 2,
-     
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
     },
-    768: {
-      slidesPerView: 2,
+    on: {
+      fromEdge: function () {
+        document
+          .querySelector(".new-arrivals-prev")
+          .classList.remove("swiper-button-disabled");
+        document
+          .querySelector(".new-arrivals-next")
+          .classList.remove("swiper-button-disabled");
+      },
     },
-    1024: {
-      slidesPerView: 4,
-    },
-    1400: {
-      slidesPerView: 5,
-    }
-  },
-      on:{
-        fromEdge: function () {
-        document.querySelector('.featured_products_prev').classList.remove('swiper-button-disabled');
-        document.querySelector('.featured_products_next').classList.remove('swiper-button-disabled');
-      }
-      }
-    });
-    });
+  });
+});
 
- window.addEventListener('load', function () {
+window.addEventListener("load", function () {
+  const top_sellers_swiper = new Swiper(".top_sellers_swiper", {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    navigation: {
+      nextEl: ".top-sellers-next",
+      prevEl: ".top-sellers-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+    on: {
+      fromEdge: function () {
+        document
+          .querySelector(".top-sellers-prev")
+          .classList.remove("swiper-button-disabled");
+        document
+          .querySelector(".top-sellers-next")
+          .classList.remove("swiper-button-disabled");
+      },
+    },
+  });
+});
+
+window.addEventListener("load", function () {
+  const featured_products_swiper = new Swiper(".featured_products_swiper", {
+    loop: false,
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".featured_products_next",
+      prevEl: ".featured_products_prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+    on: {
+      fromEdge: function () {
+        document
+          .querySelector(".featured_products_prev")
+          .classList.remove("swiper-button-disabled");
+        document
+          .querySelector(".featured_products_next")
+          .classList.remove("swiper-button-disabled");
+      },
+    },
+  });
+});
+
+window.addEventListener("load", function () {
   // Loptop Swiper
-  const loptopSwiper = new Swiper('.loptop_swiper', {
+  const loptopSwiper = new Swiper(".loptop_swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
-      nextEl: '.loptop_next',
-      prevEl: '.loptop_prev',
+      nextEl: ".loptop_next",
+      prevEl: ".loptop_prev",
     },
     breakpoints: {
       0: {
@@ -173,12 +180,12 @@ const top_sellers_swiper = new Swiper('.top_sellers_swiper', {
   });
 
   // Desktop Swiper
-  const desktopSwiper = new Swiper('.desktop_swiper', {
+  const desktopSwiper = new Swiper(".desktop_swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
-      nextEl: '.desktop_next',
-      prevEl: '.desktop_prev',
+      nextEl: ".desktop_next",
+      prevEl: ".desktop_prev",
     },
     breakpoints: {
       0: {
@@ -190,45 +197,35 @@ const top_sellers_swiper = new Swiper('.top_sellers_swiper', {
         spaceBetween: 24,
       },
       1024: {
-        slidesPerView:2,
+        slidesPerView: 2,
         spaceBetween: 24,
       },
     },
   });
 });
 
-  window.addEventListener('load', function () {
-   const itemsCathegorySwiper = new Swiper('.items_cathegory_swiper', {
-  slidesPerView: 6,
-  spaceBetween: 30, 
-  navigation: {
-    nextEl: '.items_cathegory_next',
-    prevEl: '.items_cathegory_prev',
-  },
- 
-  breakpoints: {
-    0:{
-       slidesPerView: 2,
-       
-     
+window.addEventListener("load", function () {
+  const itemsCathegorySwiper = new Swiper(".items_cathegory_swiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".items_cathegory_next",
+      prevEl: ".items_cathegory_prev",
     },
-    576: {
-      slidesPerView: 2,
-    
-        
-      
-    },
-    768: {
-      slidesPerView: 3,
-      
-      
-    },
-    1024: {
-      slidesPerView: 7,
 
-    }
-  }
-});
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 7,
+      },
+    },
   });
-
- 
+});
