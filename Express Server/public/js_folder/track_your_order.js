432d8009fabcbb2faaa985ca707c65c969e9f471
Sofia May <tempsofimae@gmail.com>
2025-08-21 17:25:18 +0800

    21/08/25 5:25pm

---
+++ Express Server/public/js_folder/track_your_order.js
@@ -0,0 +1,21 @@
+document
+  .getElementById("track_your_order_form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault(); // stop page reload
+    const number = document.getElementById("number").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const formMessage = document.getElementById("formMessage");
+
+    if (!number || !email) {
+      formMessage.textContent = "⚠️ Please fill in all fields.";
+      formMessage.className = "error showMessage";
+      return;
+    } else if (!email.includes("@")) {
+      formMessage.textContent = "⚠️ Please enter a valid email.";
+      formMessage.className = "error showMessage";
+      return;
+    } else {
+      formMessage.textContent = "✅ Order tracked successfully!";
+      formMessage.className = "success showMessage";
+    }
+  });
