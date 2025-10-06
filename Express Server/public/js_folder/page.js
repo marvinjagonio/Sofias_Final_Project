4d0b0c7c1c57f993db6b850858d1b31c3e7c77f2
Sofia May <tempsofimae@gmail.com>
2025-10-06 15:27:25 +0800

    06/10/25 3:27pm

--- Express Server/public/js_folder/page.js
+++ Express Server/public/js_folder/page.js
@@ -1,8 +1,8 @@
-const links = document.querySelectorAll('.page-link');
-  links.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      links.forEach(l => l.classList.remove('active'));
-      this.classList.add('active');
-    });
+const links = document.querySelectorAll(".page-link");
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    links.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
   });
+});
