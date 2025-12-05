e3f98b39aa9735836dec937a2cfe9447cfc99bf3
Sofia May <tempsofimae@gmail.com>
2025-12-05 14:16:42 +0800

    05/12/25

--- express_server/public/js_folder/navbar.js
+++ express_server/public/js_folder/navbar.js
@@ -1,27 +1,28 @@
+window.onload = function () {
+  history.scrollRestoration = "manual";
+  window.scrollTo(0, 0);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar");
   if (!navbar) return;
 
-  const threshold = 400;
+  const threshold = 10;
   let isVisible = true;
-  let timeout;
 
   window.addEventListener("scroll", () => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      const scrollPos = window.scrollY || window.pageYOffset;
+    const scrollPos = window.scrollY || window.pageYOffset;
 
-      if (scrollPos < threshold && isVisible) {
-        navbar.classList.add("hidden");
-        isVisible = false;
-      }
-
-      if (scrollPos >= threshold && !isVisible) {
-        setTimeout(function () {
-          navbar.classList.remove("hidden");
+    if ((scrollPos) => threshold) {
+      let timer = setTimeout(() => {
+        navbar.classList.add("fade-in");
+        setTimeout(() => {
+          navbar.classList.add("fade-out");
           isVisible = true;
-        }, 50);
-      }
-    }, 50);
+        }, 4000);
+      }, 100);
+    } else if (scrollPos != threshold) {
+      clearTimeout(timer);
+    }
   });
 });
