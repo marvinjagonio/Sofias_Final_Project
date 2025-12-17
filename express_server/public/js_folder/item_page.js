f2920596104d7a7bcba10ed2ae6f9f6d8ed3dae2
Sofia May <tempsofimae@gmail.com>
2025-12-17 20:42:06 +0800

    17/12/25 8:42 pm

--- express_server/public/js_folder/item_page.js
+++ express_server/public/js_folder/item_page.js
@@ -6,3 +6,17 @@ thumbnails.forEach((thumb) => {
     mainImage.src = thumb.src;
   });
 });
+
+const qtyInput = document.getElementById("quantityInput");
+const minusBtn = document.querySelector(".minus-btn");
+const plusBtn = document.querySelector(".plus-btn");
+
+minusBtn.addEventListener("click", () => {
+  let value = parseInt(qtyInput.value) || 1;
+  if (value > 1) qtyInput.value = value - 1;
+});
+
+plusBtn.addEventListener("click", () => {
+  let value = parseInt(qtyInput.value) || 1;
+  qtyInput.value = value + 1;
+});
