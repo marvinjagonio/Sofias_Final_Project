379a9ccbd61fb271a6d74971a533e9fe3754e6de
Sofia May <tempsofimae@gmail.com>
2026-01-02 22:49:27 +0800

    02/01/26 10:49pm

--- express_server/public/js_folder/floating_carts.js
+++ express_server/public/js_folder/floating_carts.js
@@ -189,10 +189,9 @@ document.addEventListener("DOMContentLoaded", () => {
   calculateCartTotal();
   updateCartCount();
   cartUpdateMessage();
-});
 
-// ---------- Wishlist ---------- //
-document.addEventListener("DOMContentLoaded", () => {
+  // ---------- Wishlist ---------- //
+
   let wishlistCartCount = document.querySelectorAll(".wishlist_item_1").length;
   const wishlistCartButtons = document.querySelectorAll(".wishlist");
   const wishlistCartCountDisplay = document.querySelector(
@@ -209,9 +208,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const wishlistLessElement = document.querySelector(
     "#wishlist_total_checkout .product_save"
   );
-  const wishlistCheckoutButton = document.getElementById(
-    "wishlist_cart_checkout_button"
-  );
+
   const wishlistMessage = document.querySelector(
     ".wishlist_empty_cart_message"
   );
@@ -248,7 +245,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Update the total in the checkout area
-    wishlistTotalElement.textContent = total.toFixed(2); // discounted
+    wishlistTotalElement.textContent = total.toFixed(2);
     wishlistLessElement.textContent = lessTotal.toFixed(2);
   }
 
@@ -259,12 +256,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  document.addEventListener("click", (e) => {
+    const cartBtn = e.target.closest(".cart");
+    if (!cartBtn) return;
+
+    const wishlistItem = cartBtn.closest(".wishlist_cart_item");
+    e.preventDefault();
+    removeWishlistItem(wishlistItem);
+    updateWishlistMessage();
+    wishlistCartIconChildCount--;
+    wishlistCartBadge.textContent = wishlistCartIconChildCount;
+
+    addToCart(bttn);
+    cartIconCount++;
+    cartBadge.textContent = cartIconCount;
+    updateCartCount();
+    calculateCartTotal();
+    cartUpdateMessage();
+  });
+
   // Checkout button click
 
   function updateWishlistMessage() {
-    const total = parseFloat(wishlistTotalElement.textContent);
+    const wishlistTotal = parseFloat(wishlistTotalElement.textContent);
 
-    if (wishlistCartIconChildCount <= 0 || total <= 0) {
+    if (wishlistCartIconChildCount <= 0 || wishlistTotal <= 0) {
       setTimeout(() => {
         wishlistMessage.classList.add("active");
         setTimeout(() => {
@@ -379,10 +395,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const addItem = bttn.closest(".product");
     const addItemImage = addItem.querySelector("a img").src;
     const elementName = addItem.querySelector(".product_name").textContent;
+
     const price =
       parseFloat(
         addItem.querySelector(".price")?.textContent.replace(/[^0-9.]/g, "")
       ) || 0;
+
     const discount =
       parseFloat(
         addItem
@@ -392,26 +410,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const newItem = `
     <li>
-      <div class="wishlist_cart_item wishlist_item_1">
+      <div class="wishlist_cart_item">
         <div class="wishlist_cart_item_image">
           <img src="${addItemImage}" alt="${elementName}" />
-          <button class="wishlist_remove_button">
-            <svg width="191px" height="191px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ff0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8.00386 9.41816C7.61333 9.02763 7.61334 8.39447 8.00386 8.00395C8.39438 7.61342 9.02755 7.61342 9.41807 8.00395L12.0057 10.5916L14.5907 8.00657C14.9813 7.61605 15.6144 7.61605 16.0049 8.00657C16.3955 8.3971 16.3955 9.03026 16.0049 9.42079L13.4199 12.0058L16.0039 14.5897C16.3944 14.9803 16.3944 15.6134 16.0039 16.0039C15.6133 16.3945 14.9802 16.3945 14.5896 16.0039L12.0057 13.42L9.42097 16.0048C9.03045 16.3953 8.39728 16.3953 8.00676 16.0048C7.61624 15.6142 7.61624 14.9811 8.00676 14.5905L10.5915 12.0058L8.00386 9.41816Z" fill="#ff0000"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM3.00683 12C3.00683 16.9668 7.03321 20.9932 12 20.9932C16.9668 20.9932 20.9932 16.9668 20.9932 12C20.9932 7.03321 16.9668 3.00683 12 3.00683C7.03321 3.00683 3.00683 7.03321 3.00683 12Z" fill="#ff0000"></path> </g></svg>
-          </button>
+          <button class="wishlist_remove_button">✖</button>
         </div>
+
         <div class="wishlist_cart_item_desc">
           <span>${elementName}</span>
           <span>Qty. 1</span>
         </div>
+
         <div class="wishlist_cart_item_price">
-          <b class="price">₱${price.toFixed(2)}</b>
-<span class="percent_off" style="color:red">
-  <small>₱${discount.toFixed(2)}</small>
-</span>
+          <b class="wishlist_price">₱${price.toFixed(2)}</b>
+          <span class="percent_off" style="color:red">
+            <small>₱${discount.toFixed(2)}</small>
+          </span>
         </div>
-        <button class="cart">Add to Cart</button>
+
+        <button class="cart">
+          <img src="image/cart.png" alt="Add to cart" />
+        </button>
       </div>
-    </li>`;
+    </li>
+  `;
+
     wishlistUserCart.insertAdjacentHTML("beforeend", newItem);
     calculateWishlistTotal();
   }
